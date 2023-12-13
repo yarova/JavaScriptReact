7ca6263bda364a8fd22742691eefedcb78feda8f
DESKTOP-O749V8N\Kateryna <katesun18@gmail.com>
2023-12-13 17:16:39 +0100

    Calculator style changes

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MathUtils from "./MathUtils";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+
+class Mode {
+    static Light = "light";
+    static Dark = "dark";
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -10,7 +16,7 @@ class App extends React.Component {
         this.state = {
             text: 0,
             title: "Calculator",
-            mode: "light",
+            mode: Mode.Light,
         };
         this.buttons = [
             "C",
@@ -35,8 +41,9 @@ class App extends React.Component {
         this.allowedCharacters = this.buttons.filter((i) => i != "C");
     }
 
-    onModeClick(mode) {
-        this.setState({ mode: mode });
+    onModeChange(event) {
+        var isDark = event.target.checked;
+        this.setState({ mode: isDark ? Mode.Dark : Mode.Light });
     }
 
     onEqualClick() {
@@ -87,7 +94,7 @@ class App extends React.Component {
         const buttonList = this.buttons.map((i) => (
             <button
                 onClick={() => this.onButtonClick(i)}
-                className="btn bg-light text-dark"
+                className="btn btn-calc"
                 key={i}
             >
                 {i}
@@ -95,61 +102,36 @@ class App extends React.Component {
         ));
 
         return (
-            <div
-                className={`container-fluid container-xxl pt-5 ${this.state.mode}-mode`}
-            >
-                <div
-                    className="w-100"
-                    style={{ height: 600, borderRadius: 24 }}
-                >
-                    <div className="container-fluid container-xxl mt-5 card w-50">
-                        <div className="form-check form-switch">
-                            <input
-                                className="form-check-input"
-                                type="checkbox"
-                                id="flexSwitchCheckDefault"
-                            />
-                            <label
-                                className="form-check-label"
-                                for="flexSwitchCheckDefault"
-                            ></label>
-                        </div>
-
-                        <div className="btn-group" role="group">
-                            <button
-                                type="button"
-                                className={`btn ${
-                                    this.state.mode == "light" ? "active" : ""
-                                }`}
-                                onClick={() => this.onModeClick("light")}
-                            >
-                                Light
-                            </button>
-                            <button
-                                type="button"
-                                className={`btn ${
-                                    this.state.mode == "dark" ? "active" : ""
-                                }`}
-                                onClick={() => this.onModeClick("dark")}
-                            >
-                                Dark
-                            </button>
-                        </div>
-
-                        <h3 className="text-center text-dark fw-bold fs-1">
-                            {this.state.title}
-                        </h3>
-                        <div className="mt-3 container-fluid container-xxl w-50">
-                            <div className="calculator">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="display"
-                                    onKeyDown={(e) => this.onKeyDown(e)}
-                                    onChange={(e) => this.onInputChange(e)}
-                                    value={this.state.text}
+            <div className={`${this.state.mode}-mode`}>
+                <div className="container-fluid container-xxl pt-5">
+                    <div
+                        className="w-100"
+                        style={{ height: 600, borderRadius: 24 }}
+                    >
+                        <div className="container-fluid container-xxl mt-5 card w-50">
+                            <Form className="mode-form">
+                                <Form.Check
+                                    onChange={(e) => this.onModeChange(e)}
+                                    checked={this.state.mode == Mode.Dark}
+                                    type="switch"
                                 />
-                                <div className="buttons">{buttonList}</div>
+                            </Form>
+
+                            <h3 className="text-center fs-1 mt-5">
+                                {this.state.title}
+                            </h3>
+                            <div className="mt-3 mb-5 container-fluid container-xxl w-50">
+                                <div className="calculator">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="display"
+                                        onKeyDown={(e) => this.onKeyDown(e)}
+                                        onChange={(e) => this.onInputChange(e)}
+                                        value={this.state.text}
+                                    />
+                                    <div className="buttons">{buttonList}</div>
+                                </div>
                             </div>
                         </div>
                     </div>
